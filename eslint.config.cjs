03f472ea23e3eb03b1b27af5f12e10a091f164ad
Damien Christian <damienchristian987@gmail.com>
2025-11-08 14:32:22 -0600

    Fix eslint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,23 +1,47 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jest-dom/recommended',
-    'plugin:testing-library/react',
-    'prettier',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'prettier/prettier': 'error',
+module.exports = [
+  // Global ignore patterns
+  {
+    ignores: ['dist', '.eslintrc.cjs'],
   },
-};
+
+  // Main config for source files
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:jest-dom/recommended',
+      'plugin:testing-library/react',
+      'prettier',
+    ],
+    languageOptions: {
+      env: { browser: true, es2020: true },
+      parser: require.resolve('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      'react-refresh': require('eslint-plugin-react-refresh'),
+      react: require('eslint-plugin-react'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      prettier: require('eslint-plugin-prettier'),
+      'jest-dom': require('eslint-plugin-jest-dom'),
+      'testing-library': require('eslint-plugin-testing-library'),
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+    },
+  },
+];
